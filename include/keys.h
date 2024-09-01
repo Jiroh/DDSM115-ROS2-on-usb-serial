1c784b5e10597318936275f650a568cbd9397456
Jiroh <2385486+Jiroh@users.noreply.github.com>
2024-09-01 10:16:09 +0900

    Add files via upload

---
+++ include/keys.h
@@ -0,0 +1,12 @@
+#ifndef Keys_h
+#define Keys_h
+
+namespace keys
+{
+  char wifi_ssid[20] = "";
+  char wifi_pass[20] = "";
+  char agent_ip[20] = "host_pc_IP";
+  unsigned int agent_port = 8888;
+} // namespace keys
+
+#endif /* Keys_h */
